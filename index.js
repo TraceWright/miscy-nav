b97b934e6b91db36c9418f51a33e6daf780c941a
TraceWright <traceywright27@gmail.com>
2018-11-14 19:34:22 +1000

    improve gym recognition; update gym list

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const gyms = require('./gyms.json');
 
+
+const timeReg = new RegExp(/([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/gm);
+const tiers = ["T1", "T2", "T3", "T4", "T5", "t1", "t2", "t3", "t4", "t5"];
+const listenerChannels =  ["499533390920417290", "506409230383841286", "501313521410244610", "499532605348249601"]; // ["409285420149506048"];
+const reportChannel = "511390047434702849";  // "498465708850544659"
+const errorChannel = "511472297513713674";  // "498467773610197043"
+
+
 const splitTime = (t) => {
     let time = t.split(":");
     return [parseInt(time[0]), parseInt(time[1])];
+}
+
+const calcRaidHour = (h) => {
+    return new Date().getHours() >= 12 && h < 12 ? h + 12 : h;
 }
 
 const calcHatchTime = (h, m) => {
@@ -24,69 +36,103 @@ const calcTimeToHatch = (hatchTime) => {
     return Math.floor((hatchTime - new Date().getTime()) / 60000)
 }
 
+const createErrorMsg = (tierMatch, gymAbbrvMatch, gymNameMatch, timeMatch) => {
+    return `error: Tier: ${tierMatch[0]},  ` +
+    `Gym Abbreviation: ${gymAbbrvMatch.length > 0 ? gymAbbrvMatch[0] : "invalid"} ` +
+    `OR Gym Full Name: ${!!gymNameMatch && gymNameMatch.length > 0 ? gymNameMatch[0] : "invalid"}, ` +
+    `Time: ${timeMatch[0]}`;
+}
+
 client.on("message", message => {
-    const tiers = ["T1", "T2", "T3", "T4", "T5", "t1", "t2", "t3", "t4", "t5"];
-    const channels = ["499533390920417290", "506409230383841286", "501313521410244610", "499532605348249601"];
-    let msg = message.content.split(" ");
+    
+    if (listenerChannels.includes(message.channel.id)) {
 
-    if (tiers.includes(msg[0].toString()) && channels.includes(message.channel.id)) {
-        
-        // tier always has to be the first part of the message
-        let tier = msg[0].substring(1,2);
-        
-        let dblQuote = new RegExp(/"(.*?)"/g);
-        let otherDblQuote = new RegExp(/“(.*?)”/g);
-        let sglQuote = new RegExp(/'(.*?)'/g);
-        let otherSglQuote = new RegExp(/‘(.*?)’/g);
-        let timeReg = new RegExp(/([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/gm);
+        let msg = message.content.split(" ");
+        let tierMatch = msg.filter((m, i) => { return tiers.includes(m) });
+        let timeMatch = msg.filter(m => { return m.match(timeReg) });
 
-        const matchDbl = message.content.match(dblQuote);
-        const matchOtherDbl = message.content.match(otherDblQuote);
-        const matchSgl = message.content.match(sglQuote);
-        const matchOtherSgl = message.content.match(otherSglQuote);
+        // must always have tier and time declared in incoming message
+        if (tierMatch.length > 0 && timeMatch.length > 0) {
+            
+            let dblQuote = new RegExp(/"(.*?)"/g);
+            let otherDblQuote = new RegExp(/“(.*?)”/g);
+            let sglQuote = new RegExp(/'(.*?)'/g);
+            let otherSglQuote = new RegExp(/‘(.*?)’/g);
 
-        if (!!matchDbl || !!matchSgl || !!matchOtherDbl || !!matchOtherSgl) {
-            let gym;
-            if (!!matchDbl) { 
-                gym = matchDbl[0];
-            } else if (!!matchOtherDbl) {
-                gym = matchOtherDbl[0];
-            } else if (!!matchSgl) {
-                gym = matchSgl[0];
-            } else if (!!matchOtherSgl) {
-                gym = matchOtherSgl[0];
-            }
-            let t = message.content.match(timeReg) ? splitTime(message.content.match(timeReg)[0]) : null;
-            let timeToHatch;
-            if (!!t !== false) {
-                let h = new Date().getHours() > 12 && t[0] <= 12 && t[0] > 0 ? t[0] + 12 : t[0];
-                timeToHatch = !!t && t.length === 2 ? calcTimeToHatch(calcHatchTime(h, t[1])) : null;
-            }
+            const matchDbl = message.content.match(dblQuote);
+            const matchOtherDbl = message.content.match(otherDblQuote);
+            const matchSgl = message.content.match(sglQuote);
+            const matchOtherSgl = message.content.match(otherSglQuote);
 
-            if (!!tier && !!gym && !!timeToHatch && timeToHatch > 0 && timeToHatch <= 60) {
-                client.channels.get("511390047434702849").send(`$egg ${tier} ${gym} ${timeToHatch}`); 
-            } else {
-                console.log("error: ", "tier: ", tier, " gym: ", gym, " time to hatch: ", timeToHatch);
-                client.channels.get("511472297513713674").send(`Error: Tier: ${!!tier ? tier : "invalid"}, Gym: ${!!gym ? gym : "invalid"}, Time to hatch: ${!!timeToHatch ? timeToHatch : "invalid"}`); 
-            }
-
-        } else {
-
-            let gym = msg[1].toLowerCase();
-            let timeToHatch;
-            if (msg[2].match(timeReg)) {
-                let t = splitTime(msg[2]);
-                if (!!t !== false) {
-                    let h = new Date().getHours() > 12 && t[0] <= 12 && t[0] > 0 ? t[0] + 12 : t[0];
+            if (!!matchDbl || !!matchSgl || !!matchOtherDbl || !!matchOtherSgl) {
+                let gym;
+                if (!!matchDbl) { 
+                    gym = matchDbl[0];
+                } else if (!!matchOtherDbl) {
+                    gym = matchOtherDbl[0];
+                } else if (!!matchSgl) {
+                    gym = matchSgl[0];
+                } else if (!!matchOtherSgl) {
+                    gym = matchOtherSgl[0];
+                }
+                let t = splitTime(timeMatch[0]);
+                let timeToHatch;
+                if (!!t !== false && t.length === 2) {
+                    let h = calcRaidHour(t[0]);
                     timeToHatch = calcTimeToHatch(calcHatchTime(h, t[1]));
+                } else {
+                    console.error("system error - time to hatch");
+                }
+
+                if (!!gym && !!timeToHatch && timeToHatch > 0 && timeToHatch <= 60) {
+                    client.channels.get(reportChannel).send(`$egg ${tierMatch[0]} ${gym} ${timeToHatch}`); 
+                } else {
+                    console.log("error: ", "tier: ", tierMatch[0], " gym: ", gym, " time to hatch: ", timeToHatch);
+                    client.channels.get(errorChannel).send(`Error: Tier: ${tierMatch[0]}, Gym: ${!!gym ? gym : "invalid"}, Time to hatch: ${!!timeToHatch ? timeToHatch : "invalid"}`); 
+                }
+
+            } else {
+
+                let gymNameMatch;
+                let gymAbbrvMatch;
+                let timeToHatch;
+                let unmatched = msg.filter((r, i) => { return i !== msg.indexOf(timeMatch[0]) && i !== msg.indexOf(tierMatch[0]) });
+                if (unmatched.length > 0) {
+                    const potentialGym = unmatched.join(" ").toLowerCase();
+                    gymAbbrvMatch = Object.keys(gyms).filter(g => { return g === potentialGym.toLowerCase()});
+                    gymNameMatch = gymAbbrvMatch.length <= 0 ? Object.values(gyms).filter(g => { return g === potentialGym.toLowerCase()}) : null;
+                }
+                if (gymAbbrvMatch.length > 0 || (!!gymNameMatch && gymNameMatch.length > 0)) {
+                    let t = splitTime(timeMatch[0]);
+                    if (!!t !== false) {
+                        let h = calcRaidHour(t[0]);
+                        timeToHatch = calcTimeToHatch(calcHatchTime(h, t[1]));
+                    }
+                    if ((!!timeToHatch || timeToHatch === 0) && timeToHatch >= 0 && timeToHatch < 60) {
+                        const report = !!gymNameMatch ?
+                            `$egg ${tierMatch[0].substring(1,2)} "${gymNameMatch}" ${timeToHatch}` :
+                            `$egg ${tierMatch[0].substring(1,2)} "${gyms[gymAbbrvMatch[0]]}" ${timeToHatch}` ;
+                        ;
+                        console.info(report);
+                        client.channels.get(reportChannel).send(report);
+                    } else {
+                        // Hatch time out of range
+                        const errorMessage = "Hatch time out of range " + createErrorMsg(tierMatch, gymAbbrvMatch, gymNameMatch, timeMatch);
+                        console.error(errorMessage);
+                        client.channels.get(errorChannel).send(errorMessage);
+                    }
+                } else {
+                    // gym name mismatch
+                    const errorMessage = "Gym name mismatch " + createErrorMsg(tierMatch, gymAbbrvMatch, gymNameMatch, timeMatch);
+                    console.error(errorMessage);
+                    client.channels.get(errorChannel).send(errorMessage);
                 }
             }
-            if (!!tier && !!gyms[gym] && !!timeToHatch && timeToHatch > 0 && timeToHatch <= 60) {
-                client.channels.get("511390047434702849").send(`$egg ${tier} "${gyms[gym]}" ${timeToHatch}`);
-            } else {
-                console.log("error: ", "tier: ", tier, " gym: ", gyms[gym], " time to hatch: ", timeToHatch);
-                client.channels.get("511472297513713674").send(`Error: Tier: ${!!tier ? tier : tier}, Gym: ${!!gyms[gym] ? gyms[gym] : "invalid"}, Time to hatch: ${!!timeToHatch ? timeToHatch : "invalid"}`);
-            }
+        } else {
+            // time or tier mismatch
+            // const errorMessage = `Error: Time: ${timeMatch.length > 0 ? timeMatch[0] : "invalid"}, Tier: ${tierMatch.length > 0 ? tierMatch : "invalid"}`;
+            // console.error(errorMessage);
+            // client.channels.get(errorChannel).send(errorMessage);
         }
     }
 
@@ -95,4 +141,4 @@ client.on("message", message => {
 require("log-timestamp");
 console.log("Opening Connection");
 
-client.login("token");
+client.login("NTExMzg3OTM0MDEyNzM1NDg4.Ds1ntg.4ExBITV1EcPhfBD3AMsHlX-GIvU");
